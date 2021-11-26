45ec7199bb1ca813eb79863dd15217c53e1dcc2f
Renrenrenrenrenren <75245850+Renrenrenrenrenren@users.noreply.github.com>
2021-11-26 10:19:22 +0200

    Add files via upload

---
+++ Task3.js
@@ -0,0 +1,4 @@
+let time = 50;
+let transmission = 45;
+let massage = (time>transmission)? 'Час передачі перевищений!': 'Дані перадаються у відповідності зі вказаним часом передачі сигналу';
+console.log(massage);
